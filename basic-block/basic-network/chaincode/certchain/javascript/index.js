5f4c7d175bc644bf012ce2cf00d6daa3b8961747
Keyur Canji <Keyurschool8@gmail.com>
2019-11-14 14:59:34 +0000

    Chaincode developed using hyperledger fabric, implementing simple methods required for chaincode for the context of certificates

---
+++ basic-block/basic-network/chaincode/certchain/javascript/index.js
@@ -0,0 +1,10 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+
+const Certchain = require('./lib/certchain');
+
+module.exports.Certchain = Certchain;
+module.exports.contracts = [ Certchain ];
