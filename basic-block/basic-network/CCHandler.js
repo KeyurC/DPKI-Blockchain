4912c8b91390739f2afbb07790c4ff2227d98687
Keyur Canji <Keyurschool8@gmail.com>
2019-11-19 22:01:47 +0000

    used the IBMUtil to create and add peers to channel

--- basic-block/basic-network/CCHandler.js
+++ basic-block/basic-network/CCHandler.js
@@ -6,14 +6,42 @@ const {config} = require('./config.js');
 
 orgC = new utils.orgClient(config.channelName,config.orderer0,config.Org1.peer,config.Org1.ca,config.Org1.admin);
 
+/**
+ * SetUP function
+ */
 async function setUP() {
     await orgC.login();
     await orgC.getOrgAdmin();
+    if(!await orgC.checkChannelMembership()){
+        await channelManager();
+    }
     await install();
     await instantiate();
     
 }
 
+/**
+ * Creates the channels and adds peers to it
+ */
+async function channelManager() {
+    if(!await orgC.checkChannelMembership()){
+        const channelResponse = await orgC.createChannel(config.channelConfig);
+        console.log("test");
+        if (channelResponse.status == "SUCCESS") {
+            console.log("New channel created");
+        } else {
+            console.log("Failed to create channel");
+        }
+        await orgC.joinChannel();
+        orgC.connectAndRegisterBlockEvent();
+        orgC.initialize();
+
+    }
+}
+
+/**
+ * Installs the chaincode
+ */
 async function install() {
     
     console.log("Installing");
@@ -21,6 +49,9 @@ async function install() {
     // orgC.checkInstalled()
 }
 
+/**
+ * Instantiates the chaincode
+ */
 async function instantiate() {
     if (!await orgC.checkInstantiated(config.chaincodeId,
         config.chaincodeVersion,
