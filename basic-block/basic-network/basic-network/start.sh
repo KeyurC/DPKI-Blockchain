25cc89bf52141bdc16e80c2e230099a919fd1cc9
Keyur Canji <Keyurschool8@gmail.com>
2019-11-19 22:02:31 +0000

    removed channel creation and adding peer from hyperledger fabric script

--- basic-block/basic-network/basic-network/start.sh
+++ basic-block/basic-network/basic-network/start.sh
@@ -5,6 +5,11 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 # Exit on first error, print all commands.
+# Create the channel
+#docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" peer0.org1.example.com peer channel create -o orderer.example.com:7050 -c mychannel -f /etc/hyperledger/configtx/channel.tx
+# Join peer0.org1.example.com to the channel.
+#docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" peer0.org1.example.com peer channel join -b mychannel.block
+
 set -ev
 
 # don't rewrite paths for Windows Git Bash users
@@ -21,7 +26,4 @@ export FABRIC_START_TIMEOUT=10
 #echo ${FABRIC_START_TIMEOUT}
 sleep ${FABRIC_START_TIMEOUT}
 
-# Create the channel
-docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" peer0.org1.example.com peer channel create -o orderer.example.com:7050 -c mychannel -f /etc/hyperledger/configtx/channel.tx
-# Join peer0.org1.example.com to the channel.
-docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" peer0.org1.example.com peer channel join -b mychannel.block
+
