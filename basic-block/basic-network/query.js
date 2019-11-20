05f6f43fbba43109a78e77aca8b95a2311dc8072
Keyur Canji <Keyurschool8@gmail.com>
2019-11-20 19:27:21 +0000

    cleaned up code to be used with another class

--- basic-block/basic-network/query.js
+++ basic-block/basic-network/query.js
@@ -1,29 +1,28 @@
 'use strict';
 
+const {config} = require('./config.js');
 const { FileSystemWallet, Gateway } = require('fabric-network');
 const path = require('path');
-
 const ccpPath = path.resolve(__dirname, 'basic-network', 'connection.json');
 
-async function main(wpath) {
-        // Create a new file system based wallet for managing identities.
-        const walletPath = wpath + 'wallet';
-        const wallet = new FileSystemWallet(walletPath);
-
-        // Create a new gateway for connecting to our peer node.
-        const gateway = new Gateway();
-        await gateway.connect(ccpPath, { wallet, identity: 'user1', discovery: { enabled: true, asLocalhost: true } });
-
-        // Get the network (channel) our contract is deployed to.
-        const network = await gateway.getNetwork('mychannel');
-
-        // Get the contract from the network.
-        const contract = network.getContract('abstore');
-
-        // Evaluate the specified transaction, in order to query from chaincode
-        const result = await contract.evaluateTransaction('query','a');
-    
-        console.log('Result is: '+ result.toString());
+class Query {
+        async queryCC(key) {
+                const wallet = new FileSystemWallet(config.walletPath);
+                const gateway = new Gateway();
+                await gateway.connect(ccpPath, { wallet, identity: 'user1', discovery: { enabled: true, asLocalhost: true } });
+                const network = await gateway.getNetwork(config.channelName);
+                const contract = network.getContract(config.chaincodeId);
+                const result = await contract.evaluateTransaction('query', key);
+                return result.toString();
+        }
 }
 
-main("");
+module.exports = {Query};
+
+const query = new Query();
+async function test() {
+        let value = await query.queryCC('b');
+        console.log(value);
+}
+
+test();
