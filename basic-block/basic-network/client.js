919456a4739381fe165b2fcaa7fb559e319bb8f7
Keyur Canji <Keyurschool8@gmail.com>
2020-01-26 23:05:16 +0000

    client generates csr for respective entity

---
+++ basic-block/basic-network/client.js
@@ -0,0 +1,57 @@
+"use strict";
+
+const forge = require('node-forge');
+const CA = require('./CA.js');
+const invoke = require('./invoke.js')
+
+class Client {
+  constructor(keys = "", CN = "example.org", country = "UK", state = "Middlesex",
+    locality = "isleworth", org = "DPKI", ou = "Test") {
+    this.keys = keys;
+    this.cn = CN;
+    this.country = country;
+    this.state = state;
+    this.locality = locality;
+    this.org = org;
+    this.ou = ou;
+  }
+
+  generateKeyPair() {
+    this.keys = forge.pki.rsa.generateKeyPair(1024);
+  }
+
+  generateCSR() {
+    var certificateReq = forge.pki.createCertificationRequest();
+    certificateReq.publicKey = this.keys.publicKey;
+    certificateReq.setSubject([{
+      name: 'commonName',
+      value: this.cn
+    }, {
+      name: 'countryName',
+      value: this.country
+    }, {
+      shortName: 'ST',
+      value: this.state
+    }, {
+      name: 'localityName',
+      value: this.locality
+    }, {
+      name: 'organizationName',
+      value: this.org
+    }, {
+      shortName: 'OU',
+      value: this.ou
+    }]);
+
+    certificateReq.sign(this.keys.privateKey)
+    
+    var CSR = {
+      certreq : forge.pki.certificationRequestToPem(certificateReq),
+      cn: certificateReq.subject.attributes[0].value
+    };
+    return CSR;
+  }
+
+}
+
+module.exports = Client;
