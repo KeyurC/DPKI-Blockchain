69710bc166927f35e86ee438b939abb0dc3afcc6
Keyur Canji <Keyurschool8@gmail.com>
2020-02-17 01:20:44 +0000

    updated generate script

--- basic-block/basic-network/basic-network/generate.sh
+++ basic-block/basic-network/basic-network/generate.sh
@@ -7,6 +7,7 @@
 export PATH=$GOPATH/src/github.com/hyperledger/fabric/build/bin:${PWD}/../bin:${PWD}:$PATH
 export FABRIC_CFG_PATH=${PWD}
 CHANNEL_NAME=mychannel
+CHANNEL_NAME2=mychannel2
 
 # remove previous crypto material and config transactions
 rm -fr config/*
@@ -20,7 +21,7 @@ if [ "$?" -ne 0 ]; then
 fi
 
 # generate genesis block for orderer
-configtxgen -profile OneOrgOrdererGenesis -outputBlock ./config/genesis.block
+configtxgen -profile TwoOrgOrdererGenesis -outputBlock ./config/genesis.block
 if [ "$?" -ne 0 ]; then
   echo "Failed to generate orderer genesis block..."
   exit 1
@@ -33,9 +34,23 @@ if [ "$?" -ne 0 ]; then
   exit 1
 fi
 
+# generate channel configuration transaction
+configtxgen -profile TwoOrgChannel -outputCreateChannelTx ./config/channel2.tx -channelID $CHANNEL_NAME2
+if [ "$?" -ne 0 ]; then
+  echo "Failed to generate channel configuration transaction..."
+  exit 1
+fi
+
 # generate anchor peer transaction
 configtxgen -profile OneOrgChannel -outputAnchorPeersUpdate ./config/Org1MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org1MSP
 if [ "$?" -ne 0 ]; then
   echo "Failed to generate anchor peer update for Org1MSP..."
+  exit 1
+fi
+
+# generate anchor peer transaction
+configtxgen -profile TwoOrgChannel -outputAnchorPeersUpdate ./config/Org2MSPanchors.tx -channelID $CHANNEL_NAME2 -asOrg Org2MSP
+if [ "$?" -ne 0 ]; then
+  echo "Failed to generate anchor peer update for Org2MSP..."
   exit 1
 fi
