857a5864223167878faa15a6127f40bc7fab9be3
Keyur Canji <Keyurschool8@gmail.com>
2020-03-08 19:10:55 +0000

    Updated peers and crypto material

--- basic-block/basic-network/basic-network/restart.sh
+++ basic-block/basic-network/basic-network/restart.sh
@@ -6,9 +6,8 @@ echo 'Blockchain has been stopped'
 ./teardown.sh
 wait
 echo 'Docker instances have been removed'
-rm -f -r ../wallet
 rm -f -r ../peer0.org1.example.com/
-rm -f -r ../Tests/..peer0.org1.example.com/
+rm -f -r ../peer0.org2.example.com/
 wait
 echo 'Wallets have been removed'
 ./start.sh
