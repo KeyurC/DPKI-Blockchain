ac657c61a949eabaec19d7dbc6e3d0ac53d7cd9d
Keyur Canji <Keyurschool8@gmail.com>
2020-03-14 17:37:23 +0000

    refactored code, created new class

--- basic-block/basic-network/chaincode/certchain/javascript/abstore.js
+++ basic-block/basic-network/chaincode/certchain/javascript/abstore.js
@@ -1,25 +1,23 @@
-/*
-# Copyright IBM Corp. All Rights Reserved.
-#
-# SPDX-License-Identifier: Apache-2.0
-*/
-
 const fs = require('fs');
 const shim = require('fabric-shim');
 const forge = require('node-forge');
 const os = require('os');
 const hostname = os.hostname[Symbol.toPrimitive]("String");
-
 const databaseTemplate = '[]';
+const file = new Utilities();
 
 var ABstore = class {
 
-  // Initialize the chaincode
+  /**
+   * Chaincode Init function sets up certificate database and
+   * generates a new certificate for the peer alongside private key
+   * to be used for CA responsibilities.
+   * @param {*} stub 
+   */
   async Init(stub) {
     let ret = stub.getFunctionAndParameters();
     let args = ret.params;
     let CAList = args[0];
-    let CN = args[1];
     let tmpList = JSON.parse(CAList);
     let hostname = os.hostname;
 
@@ -27,24 +25,17 @@ var ABstore = class {
     let privateKey = forge.pki.privateKeyToPem(keys.privateKey);
     let publicKey = forge.pki.publicKeyToPem(keys.publicKey);
 
-    fs.writeFile('database.json', databaseTemplate, function (err) {
-      if (err) throw err;
-      console.log('Database is created successfully.');
-    });
-
-    fs.writeFile('public.pem', publicKey, function (err) {
-      if (err) throw err;
-      console.log('Public Key File has bean created.');
-    });
-
-    fs.writeFile('private.pem', privateKey, function (err) {
-      if (err) throw err;
-      console.log('Private Key File has been created.');
-    });
+    try {
+      file.writeFile('database.json', databaseTemplate);
+      file.writeFile('public.pem', publicKey);
+      file.writeFile('private.pem', privateKey);
+    } catch (err) {
+      console.log("creation of core files failed");
+      throw (err);
+    }
 
     let certificate = forge.pki.certificateFromPem(tmpList[1].certificate);
     let pk = forge.pki.privateKeyFromPem(tmpList[1].private_key);
-
 
     let cert = forge.pki.createCertificate();
     cert.publicKey = certificate.publicKey;
@@ -73,11 +64,7 @@ var ABstore = class {
 
     let pemFormatCert = forge.pki.certificateToPem(cert);
 
-
-    fs.writeFile('certificate.pem', pemFormatCert, function (err) {
-      if (err) throw err;
-      console.log('certificate has been created.');
-    });
+    file.writeFile('certificate.pem', pemFormatCert);
 
     try {
       return shim.success();
@@ -87,9 +74,11 @@ var ABstore = class {
 
   }
 
+  /**
+   * 
+   */
   async Invoke(stub) {
     let ret = stub.getFunctionAndParameters();
-    console.info(ret);
     let method = this[ret.fcn];
     if (!method) {
       console.log('no method of name:' + ret.fcn + ' found');
@@ -97,8 +86,6 @@ var ABstore = class {
     }
     try {
       let payload = await method(stub, ret.params);
-      console.log("PAYLOAD " + payload);
-      console.log(await stub.getState('example1.org'));
       return shim.success(payload);
     } catch (err) {
       console.log(err);
@@ -106,15 +93,26 @@ var ABstore = class {
     }
   }
 
+  /**
+   * Function determines if the peer has the right to sign a 
+   * certificate request and if so generates a certificate and
+   * updates the database and adds the serial to the chaincode.
+   * The serial is a hash of the certificate subject so any modification
+   * of the subject causes the process to fail.  
+   * @param {*} stub 
+   * @param {*} args 
+   */
   async invoke(stub, args) {
     let peer = args[0];
-    let certreq;
     let hashed = args[2];
+
+    let certreq;
     try {
       certreq = forge.pki.certificationRequestFromPem(args[1])
     } catch (error) {
       console.error("Failed to obtain/convert CSR" + error)
     }
+
     if (peer == hostname) {
       let privateKey = await new Promise((resolve, reject) => {
         fs.readFile('private.pem', 'utf8', function (err, data) {
@@ -147,69 +145,49 @@ var ABstore = class {
       cert.setIssuer(signCert.subject.attributes);
       cert.sign(pk);
 
-      let database = await new Promise((resolve, reject) => {
-        fs.readFile('database.json', 'utf8', function (err, data) {
-          if (err) {
-            reject(err);
-          }
-          resolve(data);
-        });
-      });
-
-      let insert = '{"SerialNo":"' + hashed + '","Certificate":"' + forge.pki.certificateToPem(cert).replace(/\n|\r/g, '') + '"}';
-
-      let currentData = database.substring(0, database.length - 1);
-      let newEntry = insert;
-      let updated;
-
-      if (databaseTemplate.length === database.length) {
-        updated = currentData + newEntry + ']';
-      } else {
-        updated = currentData + ',' + newEntry + ']';
-      }
-
-      console.log(hostname);
-      console.log(peer);
-
-
-      fs.writeFile('database.json', updated, function (err) {
-        if (err) throw err;
-        console.log('File is created successfully.');
-      });
+      await file.updateDatabase(hashed,cert);
     }
 
     await stub.putState(certreq.subject.attributes[0].value.toString(), Buffer.from(hashed));
   }
 
-  async verify(stub, args) {
-    let rootObj = JSON.parse(await stub.getState("ROOTCA"));
-    let rootcert = forge.pki.certificateFromPem(rootObj.certificate);
+  // async verify(stub, args) {
+  //   let rootObj = JSON.parse(await stub.getState("ROOTCA"));
+  //   let rootcert = forge.pki.certificateFromPem(rootObj.certificate);
 
-    let cert = forge.pki.certificateFromPem(args[0]);
+  //   let cert = forge.pki.certificateFromPem(args[0]);
 
-    let issuer = cert.issuer.attributes[0].value;
-    let issuerObj = null;
-    try {
-      issuerObj = JSON.parse(await stub.getState(issuer));
-    } catch (err) {
-      console.log("Issuer does not exist or JSON Parsing fault");
-      return Buffer.from("Certificate does not belong to us");
-    }
+  //   let issuer = cert.issuer.attributes[0].value;
+  //   let issuerObj = null;
+  //   try {
+  //     issuerObj = JSON.parse(await stub.getState(issuer));
+  //   } catch (err) {
+  //     console.log("Issuer does not exist or JSON Parsing fault");
+  //     return Buffer.from("Certificate does not belong to us");
+  //   }
 
-    let issuerCert = forge.pki.certificateFromPem(issuerObj.certificate);
+  //   let issuerCert = forge.pki.certificateFromPem(issuerObj.certificate);
 
-    let certVerified = issuerCert.verify(cert);
-    if (certVerified) {
-      if (!rootcert.verify(issuerCert)) {
-        return Buffer.from("Certificate does not belong to us");
-      }
-    } else {
-      return Buffer.from("Certificate does not belong to us");
-    }
-    return Buffer.from("Certificate belongs to us");
-  }
+  //   let certVerified = issuerCert.verify(cert);
+  //   if (certVerified) {
+  //     if (!rootcert.verify(issuerCert)) {
+  //       return Buffer.from("Certificate does not belong to us");
+  //     }
+  //   } else {
+  //     return Buffer.from("Certificate does not belong to us");
+  //   }
+  //   return Buffer.from("Certificate belongs to us");
+  // }
 
-  // query callback representing the query of a chaincode
+  /**
+   * Function uses the serial retrieved from the chaincode as a key
+   * in order to query the database and return the certificate.
+   * Using the serial from the chaincode validates the certificate
+   * Any database lacking the serial will return the serial as not
+   * all peers will have every certificate. 
+   * @param {*} stub 
+   * @param {*} args 
+   */
   async query(stub, args) {
     if (args.length != 1) {
       throw new Error('Incorrect number of arguments. Expecting name of the person to query')
@@ -225,14 +203,7 @@ var ABstore = class {
       throw new Error(JSON.stringify(jsonResp));
     }
 
-    let database = await new Promise((resolve, reject) => {
-      fs.readFile('database.json', 'utf8', function (err, data) {
-        if (err) {
-          reject(err);
-        }
-        resolve(data);
-      });
-    });
+    let database = await file.readFile("database.json")
 
     let db = JSON.parse(database);
     let keys = Object.keys(db);
@@ -251,6 +222,70 @@ var ABstore = class {
     console.info(jsonResp);
     return Avalbytes;
   }
+
 };
 
+/**
+ * The class contains basic functions used by 
+ * the chaincode numerous times.
+ */
+function Utilities() {
+
+  /** 
+   * Generates a new file
+   * @param name name of the file created
+   * @param input input of the file
+  */
+  this.writeFile = function (name, input) {
+    fs.writeFile(name.toString(), input, function (err) {
+      if (err) throw err;
+      console.log(name + ' is created successfully.');
+    });
+
+  }
+  
+  /** 
+   * reads exisiting file
+   * @param name name of the file
+  */
+  this.readFile = async function (name) {
+    let read = await new Promise((resolve, reject) => {
+      fs.readFile(name, 'utf8', function (err, data) {
+        if (err) {
+          reject(err);
+        }
+        resolve(data);
+      });
+    });
+    return read;
+  }
+
+  /**
+   * The function reads and updates the existing database
+   * @param hashed serial
+   * @param cert certificate generated from certificate request.
+   */
+  this.updateDatabase = async function (hashed,cert) {
+    let database = await this.readFile("database.json")
+
+    console.log("DATA",database);
+    
+    let insert = '{"SerialNo":"' + hashed + '","Certificate":"' + forge.pki.certificateToPem(cert).replace(/\n|\r/g, '') + '"}';
+    let currentData = database.substring(0, database.length - 1);
+    let newEntry = insert;
+    let updated;
+
+    if (databaseTemplate.length === database.length) {
+      updated = currentData + newEntry + ']';
+    } else {
+      updated = currentData + ',' + newEntry + ']';
+    }
+
+    this.writeFile('database.json', updated)
+
+  }
+
+}
+
 shim.start(new ABstore());
+
