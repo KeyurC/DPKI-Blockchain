ac0aa8f5dbe9190660609fb504e6252675bd1f2b
Keyur Canji <Keyurschool8@gmail.com>
2020-03-17 14:01:19 +0000

    updated revocation chaincode, to store information about revoked certificates, retrieve all revoked certificates etc

--- basic-block/basic-network/chaincode/revchain/javascript/revocation.js
+++ basic-block/basic-network/chaincode/revchain/javascript/revocation.js
@@ -1,29 +1,26 @@
-/*
-# Copyright IBM Corp. All Rights Reserved.
-#
-# SPDX-License-Identifier: Apache-2.0
-*/
-
 const shim = require('fabric-shim');
-const forge = require('node-forge');
+const fs = require('fs');
+let template = "[]";
 
+/**
+ * The class handles the certificate revocation aspect
+ * of the PKI system.
+ */
 var Revocation = class {
 
-  // Initialize the chaincode
+  /**
+   * Instantiate function generates key file to store all keys
+   * of the blockchain in.
+   * @param {} stub 
+   */
   async Init(stub) {
-    let ret = stub.getFunctionAndParameters();
-    let args = ret.params;
-    let Hash = args[0];
-    let CN = args[1];
+    fs.writeFile("keys", template, function (err) {
+      if (err) throw err;
+      console.log('key is created successfully.');
+    });
 
-    try {
-      await stub.putState(CN, Buffer.from(Hash));
-      return shim.success();
-    } catch (err) {
-      return shim.error(err);
-    }
+    return shim.success();
   }
-
 
   async Invoke(stub) {
     let ret = stub.getFunctionAndParameters();
@@ -42,14 +39,101 @@ var Revocation = class {
     }
   }
 
+  /**
+   * Invoke function adds relevent information about certificate
+   * to the blockchain using the common name as the key.
+   * @param {*} stub 
+   * @param {*} args 
+   */
   async invoke(stub, args) {
+    let date = Date.now();
     let CN = args[0];
-    let Hash = args[1];
+    let serial = args[1];
+    let reason = args[2];
 
-    await stub.putState(CN, Buffer.from(Hash));
+    let database = await new Promise((resolve, reject) => {
+      fs.readFile("keys", 'utf8', function (err, data) {
+        if (err) {
+          reject(err);
+        }
+        resolve(data);
+      });
+    });
+
+    let insert = '"' + CN + '"';
+    let currentData = database.substring(0, database.length - 1);
+    let newEntry = insert;
+    let updated;
+
+    if (template.length === database.length) {
+      updated = currentData + newEntry + ']';
+    } else {
+      updated = currentData + ',' + newEntry + ']';
+    }
+
+    console.log(updated);
+
+    let certificateRevocation = {
+      serial: serial,
+      date: date,
+      reason: reason
+    }
+
+    fs.writeFile("keys", updated, function (err) {
+      if (err) throw err;
+      console.log('database is created successfully.');
+    });
+
+    await stub.putState(CN, Buffer.from(JSON.stringify(certificateRevocation)));
   }
 
-  // query callback representing the query of a chaincode
+  /**
+   * The function uses the key file to retrieve all revoked certificates
+   * within the chaincode.
+   * @param {*} stub 
+   * @param {*} args 
+   */
+  async getAllRevokedCertificates(stub, args) {
+    let filter = args[0];
+
+    let keys = await new Promise((resolve, reject) => {
+      fs.readFile("keys", 'utf8', function (err, data) {
+        if (err) {
+          reject(err);
+        }
+        resolve(data);
+      });
+    });
+
+    let db = JSON.parse(keys);
+
+    let list = [];
+
+    for (let i = 0; i < db.length; i++) {
+      console.log(db[i]);
+      let tmp = await stub.getState(db[i]);
+      let revoke = JSON.parse(tmp.toString());
+      let tmpList = {
+        domain: db[i],
+        reocation: tmp.toString()
+      };
+
+      if (typeof filter != 'undefined') {
+        if (revoke.reason == filter) {
+          list.push(tmpList);
+        }
+      } else {
+        list.push(tmpList);
+      }
+    }
+
+    return Buffer.from(JSON.stringify(list));
+
+  }
+
+  /**
+   * Queries a singular revoked certificate 
+   */
   async query(stub, args) {
     if (args.length != 1) {
       throw new Error('Incorrect number of arguments. Expecting name of the person to query')
