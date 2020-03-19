2accd0b1a02e2378206fb78844ca1b12e572f199
Keyur Canji <Keyurschool8@gmail.com>
2020-03-19 15:31:06 +0000

    altered config so it could be used for a network grapth to be constructed from it

--- basic-block/basic-network/utilities/config.js
+++ basic-block/basic-network/utilities/config.js
@@ -1,46 +1,22 @@
 const { readFileSync } = require('fs');
 const path = require('path');
+const fs = require('fs');
 
-
-
-// const basePath = resolve(__dirname, '../../certs');
-// const readCryptoFile =
-// filename => readFileSync(resolve(basePath)).toString();
 const config = {
-  isCloud: false,
   orderer0: {
     hostname: 'orderer.example.com',
     url: 'grpcs://localhost:7050',
-    pem : readFileSync(path.resolve(__dirname,'../','basic-network/crypto-config/ordererOrganizations/example.com/orderers/orderer.example.com/tls/ca.crt')).toString()
+    pem: readFileSync(path.resolve(__dirname, '../', 'basic-network/crypto-config/ordererOrganizations/example.com/orderers/orderer.example.com/tls/ca.crt')).toString()
   },
   Org1: {
-    // endorsementpolicy = {
-    //   "identities": [
-    //     {
-    //       "role": {
-    //         "name": "member",
-    //         "mspId": "Org1MSP"
-    //       }
-    //     }
-    //   ],
-    //   "policy": {
-    //     "2-of": [
-    //       {
-    //         "signed-by": 0
-    //       },
-    //       {
-    //         "signed-by": 0
-    //       },
-    //     ]
-    //   }
-    // },
     channel: {
       channelName: 'mychannel',
       channelConfig: readFileSync(path.resolve(__dirname, '../', 'basic-network/config/channel.tx')),
+      node: 'true'
     },
     chaincode: {
       chaincodeId: 'abstore',
-      chaincodeVersion: 'v3',
+      chaincodeVersion: 'v16',
       chaincodePath: '../chaincode/certchain/javascript/'
     },
     peer: {
@@ -48,14 +24,16 @@ const config = {
         hostname: 'peer0.org1.example.com',
         url: 'grpcs://localhost:7051',
         eventHubUrl: 'grpc://localhost:7053',
-        pem: readFileSync(path.resolve(__dirname,'../basic-network/crypto-config/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt')).toString()
+        pem: readFileSync(path.resolve(__dirname, '../basic-network/crypto-config/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt')).toString(),
+
       },
       peer1: {
         hostname: 'peer1.org1.example.com',
         url: 'grpcs://localhost:9051',
         eventHubUrl: 'grpc://localhost:9053',
-        pem: readFileSync(path.resolve(__dirname,'../basic-network/crypto-config/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/tls/ca.crt')).toString()
-      }
+        pem: readFileSync(path.resolve(__dirname, '../basic-network/crypto-config/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/tls/ca.crt')).toString(),
+      },
+      node: "multiple"
     },
     ca: {
       hostname: 'ca0.example.com',
@@ -69,29 +47,10 @@ const config = {
   },
 
   Org2: {
-    // endorsementpolicy = {
-    //   "identities": [
-    //     {
-    //       "role": {
-    //         "name": "member",
-    //         "mspId": "Org2MSP"
-    //       }
-    //     }
-    //   ],
-    //   "policy": {
-    //     "2-of": [
-    //       {
-    //         "signed-by": 0
-    //       },
-    //       {
-    //         "signed-by": 0
-    //       },
-    //     ]
-    //   }
-    // },
     channel: {
       channelName: 'mychannel2',
       channelConfig: readFileSync(path.resolve(__dirname, '../', 'basic-network/config/channel2.tx')),
+      node: 'true'
     },
     chaincode: {
       chaincodeId: 'revocation',
@@ -103,8 +62,9 @@ const config = {
         hostname: 'peer0.org2.example.com',
         url: 'grpcs://localhost:8051',
         eventHubUrl: 'grpc://localhost:8053',
-        pem: readFileSync(path.resolve(__dirname,'../basic-network/crypto-config/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt')).toString()
-      }
+        pem: readFileSync(path.resolve(__dirname, '../basic-network/crypto-config/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt')).toString()
+      },
+      node: "multiple"
     },
     ca: {
       hostname: 'ca1.example.com',
@@ -118,26 +78,6 @@ const config = {
   }
 };
 
-// if (process.env.LOCALCONFIG) {
-//   config.orderer0.url = 'grpcs://localhost:7050';
-
-//   config.insuranceOrg.peer.url = 'grpcs://localhost:7051';
-//   config.shopOrg.peer.url = 'grpcs://localhost:8051';
-//   config.repairShopOrg.peer.url = 'grpcs://localhost:9051';
-//   config.policeOrg.peer.url = 'grpcs://localhost:10051';
-
-//   config.insuranceOrg.peer.eventHubUrl = 'grpcs://localhost:7053';
-//   config.shopOrg.peer.eventHubUrl = 'grpcs://localhost:8053';
-//   config.repairShopOrg.peer.eventHubUrl = 'grpcs://localhost:9053';
-//   config.policeOrg.peer.eventHubUrl = 'grpcs://localhost:10053';
-
-//   config.insuranceOrg.ca.url = 'https://localhost:7054';
-//   config.shopOrg.ca.url = 'https://localhost:8054';
-//   config.repairShopOrg.ca.url = 'https://localhost:9054';
-//   config.policeOrg.ca.url = 'https://localhost:10054';
-// }
-
 const DEFAULT_CONTRACT_TYPES = [];
-
 
 module.exports = { config, DEFAULT_CONTRACT_TYPES }
